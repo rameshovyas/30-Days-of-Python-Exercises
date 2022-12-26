d0b64f69bc9393ffed26ed40c11174d16cc4c4db
Ramesh Vyas <rameshovyas@gmail.com>
2022-12-27 04:58:51 +0530

    Solution to exercise 11 to 21

--- Day_04_Strings/day_04.py
+++ Day_04_Strings/day_04.py
@@ -43,3 +43,50 @@ print(_first)
 # Check if Coding For All string contains a word Coding using the method index, find or other methods.
 print(True) if(company.index("Coding") > -1 ) else print(False)
 print(True) if(company.find("Coding") > -1 ) else print(False)
+
+# Replace the word coding in the string 'Coding For All' to Python.
+company = company.replace("Coding","Python")
+print(company)
+
+# Change Python for All to Python for Everyone using the replace method or other methods.
+company = company.replace("All","Everyone")
+print(company)
+
+# Split the string 'Coding For All' using space as the separator (split()) .
+str = 'Coding For All'
+print(str.split(' '))
+
+# "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" split the string at the comma.
+str = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" 
+print(str.split(","))
+
+# What is the character at index 0 in the string Coding For All.
+str = 'Coding For All'
+print(str[0])
+
+# What is the last index of the string Coding For All.
+print(len(str)-1)
+
+# What character is at index 10 in "Coding For All" string.
+print(str[10])
+
+# Create an acronym or an abbreviation for the name 'Python For Everyone'.
+str = 'Python For Everyone'
+abr=''
+for s in str.split(' '):
+    abr = abr + s[0]
+print(abr)
+
+# Create an acronym or an abbreviation for the name 'Coding For All'.
+str = 'Coding For All'
+abr=''
+for s in str.split(' '):
+    abr = abr + s[0]
+print(abr)
+
+# Use index to determine the position of the first occurrence of C in Coding For All.
+str = 'Coding For All'
+print(str.index('C'))
+
+# Use index to determine the position of the first occurrence of F in Coding For All.
+print(str.index('F'))

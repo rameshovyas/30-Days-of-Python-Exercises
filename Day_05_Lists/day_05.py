81bccb2b0ffe2662472c11f314a1e71aae5dc7d6
Ramesh Vyas <rameshovyas@gmail.com>
2022-12-27 08:13:19 +0530

    Added Day 05

---
+++ Day_05_Lists/day_05.py
@@ -0,0 +1,8 @@
+# Declare an empty list
+myList =[]
+
+# Declare a list with more than 5 items
+gods = ['Shiva', 'Rama', 'Krishna', 'Hanumana', 'Vishnu', 'Ganesha']
+
+# Find the length of your list
+print(len(gods))

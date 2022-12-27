d15e89d344c0d9704b98f6c4a9b23d864ebb144e
Ramesh Vyas <rameshovyas@gmail.com>
2022-12-28 04:09:34 +0530

    Solution to Day 5 exercise 1 to 10

--- Day_05_Lists/day_05.py
+++ Day_05_Lists/day_05.py
@@ -6,3 +6,34 @@ gods = ['Shiva', 'Rama', 'Krishna', 'Hanumana', 'Vishnu', 'Ganesha']
 
 # Find the length of your list
 print(len(gods))
+
+# Get the first item, the middle item and the last item of the list
+print(gods[0])
+print(gods[len(gods)-1])
+
+# Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
+mixed_data_types = ['Mr. Human', 15, 6, "Un Married", "Planet Earth"]
+print(mixed_data_types)
+
+# Declare a list variable named it_companies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
+it_companies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"]
+
+# Print the list using print()
+print(it_companies)
+
+# Print the number of companies in the list
+print(len(it_companies))
+
+# Print the first, middle and last company
+first = it_companies[0]
+last = it_companies[len(it_companies)-1]
+middle = it_companies[int(len(it_companies)/2)]
+
+print(first)
+print(middle)
+print(last)
+
+# Print the list after modifying one of the companies
+it_companies[0] = "Firmonyx"
+print(it_companies)
+

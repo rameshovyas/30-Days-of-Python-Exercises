6e9a8775e2a009822be0c5c6c9648c651a6c7793
Ramesh Vyas <rameshovyas@gmail.com>
2023-01-08 12:00:39 +0530

    Added Day 06

---
+++ Day_06_Tuples/day_06.py
@@ -0,0 +1,16 @@
+
+# Create an empty tuple
+siblings = ()
+
+# Create a tuple containing names of your sisters and your brothers (imaginary siblings are fine)
+brothers = ("Rama", "Krishna", "Shiva", "Hanumana")
+sisters = ("Sita","Laxmi", "Parvati", "Saraswati")
+
+# Join brothers and sisters tuples and assign it to siblings
+siblings = brothers + sisters
+
+print(siblings)
+
+# How many siblings do you have?
+print("I have %s siblings" % (len(siblings)))
+# Modify the siblings tuple and add the name of your father and mother and assign it to family_members

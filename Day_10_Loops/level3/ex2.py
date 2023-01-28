6669dc99433c9e35913f5e45b0a47bc66d7d8fdf
Ramesh Vyas <rameshovyas@gmail.com>
2023-01-28 13:47:06 +0530

    Day 10 level3 exercise 2

---
+++ Day_10_Loops/level3/ex2.py
@@ -0,0 +1,4 @@
+# This is a fruit list, ['banana', 'orange', 'mango', 'lemon'] reverse the order using loop.
+fruits = ['banana', 'orange', 'mango', 'lemon']
+for i in range(len(fruits)-1,-1, -1):
+    print(fruits[i])

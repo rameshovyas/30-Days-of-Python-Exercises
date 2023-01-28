981ceab9407e5a77bb9de03c11daf7aa15a96893
Ramesh Vyas <rameshovyas@gmail.com>
2023-01-28 12:55:46 +0530

    Day 10 Level 1 question 2

---
+++ Day_10_Loops/level1/ex2.py
@@ -0,0 +1,10 @@
+# Iterate 10 to 0 using for loop, do the same using while loop.
+print("Demonstarting for loop")
+for i in range(10,-1,-1):
+    print (i)
+
+print("Demonstarting while loop")
+i = 10
+while i >= 0:
+    print(i)
+    i = i -1

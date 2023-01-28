3f3f0d3b6222f4bc0dbe5c66004048ba805529bb
Ramesh Vyas <rameshovyas@gmail.com>
2023-01-28 13:22:20 +0530

    Day 10 Level1 Exercise 6

---
+++ Day_10_Loops/level1/ex6.py
@@ -0,0 +1,4 @@
+# Iterate through the list, ['Python', 'Numpy','Pandas','Django', 'Flask'] using a for loop and print out the items.
+techs = ['Python', 'Numpy','Pandas','Django', 'Flask']
+for tech in techs:
+    print (tech)

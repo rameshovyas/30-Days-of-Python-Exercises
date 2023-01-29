112ef9165868845c2e814f1f6c40360a156aa67a
Ramesh Vyas <rameshovyas@gmail.com>
2023-01-29 18:30:06 +0530

    Removed extra spacing in comment

--- Day_13_List_Comprehension/ex3.py
+++ Day_13_List_Comprehension/ex3.py
@@ -1,4 +1,5 @@
-# Using list comprehension create the following list of tuples
+#Using list comprehension create the following list of tuples
+
 lst = [(a,b,a,a**2,a**3,a**4,a**5) 
         for a in range(0,11)
         for b in [1]

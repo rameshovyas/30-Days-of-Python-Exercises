1ab32ee946442e8414f0ed8135368af6598b1471
Ramesh Vyas <rameshovyas@gmail.com>
2023-01-29 08:04:37 +0530

    Day 11 Level1 Exercise 2

---
+++ Day_11_Functions/Level1/ex2.py
@@ -0,0 +1,7 @@
+# Area of a circle is calculated as follows: area = π x r x r. Write a function that calculates area_of_circle.
+PI = 3.14
+def area_of_circle(r):    
+    return PI * (r**2)
+
+
+print(area_of_circle(2.5))

a6fcf1eeb507a3c806faff1558b6c6fb3f01867e
Ramesh Vyas <rameshovyas@gmail.com>
2023-01-30 12:16:06 +0530

    Day 16 exercise 1

---
+++ Day_16_DateTime/ex1.py
@@ -0,0 +1,9 @@
+# Get the current day, month, year, hour, minute and timestamp from datetime module
+from datetime import datetime
+print("Current Day : ", datetime.now().day)
+print("Current Month : ", datetime.now().month)
+print("Current Year : ", datetime.now().year)
+print("Current Hour : ", datetime.now().hour)
+print("Current Minute : ", datetime.now().minute)
+print("Current Timestamp : ", datetime.now().timestamp())
+

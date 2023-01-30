aeb6a05b4783528eb339b0510d74ddf52db99074
Ramesh Vyas <rameshovyas@gmail.com>
2023-01-30 18:48:15 +0530

    Day 19 level2 ex 4 : find all incoming email address from email exchange server big data

---
+++ Day_19_File_Handling/Level2/ex4.py
@@ -0,0 +1,10 @@
+# Extract all incoming email addresses as a list from the email_exchange_big.txt file.
+f = open("email_exchanges_big.txt", "r")
+lines = f.readlines()
+email_address = []
+for line in lines:
+    if(line.startswith("From")):
+        
+        email_address.append(line.split()[1])
+
+print(email_address)

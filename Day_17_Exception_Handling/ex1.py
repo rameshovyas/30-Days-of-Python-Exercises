73c1eb5df52683841ad213fd6ec22d2a341f0923
Ramesh Vyas <rameshovyas@gmail.com>
2023-01-30 12:40:58 +0530

    Day 17 ex1 unpacking list

---
+++ Day_17_Exception_Handling/ex1.py
@@ -0,0 +1,7 @@
+# Unpack the first five countries and store them in a variable nordic_countries, 
+# store Estonia and Russia in es, and ru respectively.
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+*first_five,es,ru = names
+print(first_five)
+print(es)
+print(ru)

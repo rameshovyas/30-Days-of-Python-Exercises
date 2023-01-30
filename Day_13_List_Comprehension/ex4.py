b7e90fbca9968f29312ad7fa0f197184c23f7516
Ramesh Vyas <rameshovyas@gmail.com>
2023-01-30 15:16:19 +0530

    changes

--- Day_13_List_Comprehension/ex4.py
+++ Day_13_List_Comprehension/ex4.py
@@ -2,6 +2,7 @@
 countries = [[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]]
 # output Required
 # [['FINLAND','FIN', 'HELSINKI'], ['SWEDEN', 'SWE', 'STOCKHOLM'], ['NORWAY', 'NOR', 'OSLO']]
+
 new_list = [[item[0].upper(),item[0].upper()[:3],item[1].upper()]  for country in countries for item in country ]
 print(new_list)
 

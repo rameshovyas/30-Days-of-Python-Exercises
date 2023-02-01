92bdfcbd94fdcc194dc48118f48ccc45dc3f5be8
Ramesh Vyas <rameshovyas@gmail.com>
2023-02-01 11:18:05 +0530

    Added class skelaton

---
+++ Day_21_Classes_Objects/ex1.py
@@ -0,0 +1,29 @@
+class Statistics:
+    def count(self):
+        print("Count")
+    def sum(self):
+        print("sum")
+    def min(self):
+        print("min")
+    def max(self):
+        print("max")
+    def range(self):
+        print("range")
+    def mean(self):
+        print("mean")
+    def median(self):
+        print("median")
+    def mode(self):
+        print("mode")
+    def std(self):
+        print("std")
+    def var(self):
+        print("var")
+    def freq_dist(self):
+        print("freq_dist")
+        
+        
+
+stat = Statistics()
+
+stat.freq_dist()

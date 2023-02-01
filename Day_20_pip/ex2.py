0b12d0c950447067306ac14ed8dc3f239337d012
Ramesh Vyas <rameshovyas@gmail.com>
2023-02-01 08:59:33 +0530

    Frequency table of country and cat breeds added

--- Day_20_pip/ex2.py
+++ Day_20_pip/ex2.py
@@ -1,28 +1,31 @@
 # Read the cats API and cats_api = 'https://api.thecatapi.com/v1/breeds' and find : 
 import requests
 import statistics
+
 url = 'https://api.thecatapi.com/v1/breeds'
 try:
     print()
     print("Fetching data from %s, please wait...." % (url))
     response = requests.get(url)
     if(response.status_code == 200):
-        cats_data = response.json()
-        
+        cats_data = response.json()      
         
         weight = []
         lifespan = []
+        freq_table = []
+
         for cat in cats_data:
           # weight  
           if("weight" in cat):
             if("metric" in cat["weight"]):
                 low,hi = cat["weight"]['metric'].split("-")
                 weight.append((int(low) + int(hi))/2) 
+          
           #lifespan      
           if("life_span" in cat):
                 low,hi = cat["life_span"].split("-")
                 lifespan.append((int(low) + int(hi))/2) 
-
+                    
         # the min, max, mean, median, standard deviation of cats' weight in metric units.
         min_weight = min(weight)
         max_weight = max(weight)   
@@ -57,7 +60,16 @@ try:
         print()
         print("******************************************************")
 
+        # Create a frequency table of country and breed of cats
+        print("** Frequency table of country and breed of cats *******")
+        print()
+        countries = set((cat['origin']) for cat in cats_data)
+        for country in countries:
+            freq_table.append((country, len([cat['name'] for cat in cats_data if cat['origin'] == country])))
+        print(freq_table)    
+        print()
+        print("******************************************************")
     else:
         print("Error loading data : Response %s" % (response.status_code))    
-except :
-    print("Something went wrong....")
+except Exception as e:
+    print(e)

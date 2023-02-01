b2c96d696cb181b80fc9f9aec2c3031846a04c74
Ramesh Vyas <rameshovyas@gmail.com>
2023-02-01 12:06:47 +0530

    added mean , median methods

--- Day_21_Classes_Objects/ex1.py
+++ Day_21_Classes_Objects/ex1.py
@@ -33,9 +33,30 @@ class Statistics:
         return None
 
     def mean(self):
-        print("mean")
+        if(len(self.data)>0):
+            return (sum(self.data) / len(self.data))    
+        return None
+
     def median(self):
-        print("median")
+        if((self.data) and (len(self.data)>0)):
+            # First sort data in ascending order
+            tmp_data = sorted(self.data)
+            n = len(tmp_data)
+            
+            # For odd number of data
+            if(n%2 !=0):
+                return (tmp_data[(n+1) // 2])
+            
+            # For even number of data    
+            else:
+                a = tmp_data[n // 2]
+                b = tmp_data[(n // 2) + 1]
+                return ((a+b)/2)
+                
+
+            
+
+
     def mode(self):
         print("mode")
     def std(self):
@@ -50,5 +71,5 @@ ages = [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 
 
 stat = Statistics(ages)
 
-print(stat.range())
+print(stat.median())
 

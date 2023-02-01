a041e76e3496f33c63af6dc12b41563ce48f1352
Ramesh Vyas <rameshovyas@gmail.com>
2023-02-01 11:01:42 +0530

    fetched table data

--- Day_20_pip/ex3.py
+++ Day_20_pip/ex3.py
@@ -6,4 +6,12 @@ url = "https://archive.ics.uci.edu/ml/datasets.php"
 
 response = requests.get(url).text
 soup = BeautifulSoup(response,"html.parser")
-print(soup)
+
+# Creating list with all tables
+tables = soup.find_all('table')
+
+#  Looking for the table with the cellpadding = 5
+table = soup.find('table', cellpadding=5)
+
+columns = table.find_all('tr') 
+print(columns)

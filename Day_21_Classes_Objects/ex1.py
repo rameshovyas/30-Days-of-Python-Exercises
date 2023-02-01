ade65759afde91024c13a74f78b75cf5b31dcd23
Ramesh Vyas <rameshovyas@gmail.com>
2023-02-01 12:49:46 +0530

    Frequency distribution added

--- Day_21_Classes_Objects/ex1.py
+++ Day_21_Classes_Objects/ex1.py
@@ -76,13 +76,25 @@ class Statistics:
         return None
 
     def freq_dist(self):
-        print("freq_dist")
+        if(self.data):
+            return set([(x, self.data.count(x)) for x in self.data])
+        return None    
         
         
 ages = [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37, 31, 34, 24, 33, 29, 26]
 
 lst = [1, 2, 3, 4, 5]
-stat = Statistics(ages)
-
-print(stat.std())
+data = Statistics(ages)
+print("************Result*************")
+print('Count:', data.count()) # 25
+print('Sum: ', data.sum()) # 744
+print('Min: ', data.min()) # 24
+print('Max: ', data.max()) # 38
+print('Range: ', data.range()) # 14
+print('Mean: ', data.mean()) # 30
+print('Median: ', data.median()) # 29
+print('Mode: ', data.mode()) # {'mode': 26, 'count': 5}
+print('Standard Deviation: ', data.std()) 
+print('Variance: ', data.var()) 
+print('Frequency Distribution: ', data.freq_dist())
 

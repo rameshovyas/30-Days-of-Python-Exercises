0e9a077ced21e1827661c9ef6045aa52176f15f0
Ramesh Vyas <rameshovyas@gmail.com>
2023-02-02 12:43:02 +0530

    Various mathematical operations on numpy array

---
+++ Day_24_Statistical_Analysis/nump_maths.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+my_list = [1,2,3,4,5]
+
+np_array = np.array(my_list)
+
+two_plus_original = np_array + 2
+two_minus_original = np_array - 2
+two_times_original = np_array * 2
+two_div_original = np_array / 2
+print(np_array)
+print(two_plus_original)
+print(two_minus_original)
+print(two_times_original)
+print(two_div_original)
+

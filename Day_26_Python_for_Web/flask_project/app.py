2a3a4e71ac22844c1287ff166dbd97df02cde494
Ramesh Vyas <rameshovyas@gmail.com>
2023-02-03 07:32:31 +0530

    Added template for home page

--- Day_26_Python_for_Web/flask_project/app.py
+++ Day_26_Python_for_Web/flask_project/app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # Create home route
 @app.route('/') 
 def home ():
-    return '<h1>Welcome</h1>'
+    return render_template('index.html')
+    
 
 # Spin the server
 if __name__ == "__main__":
